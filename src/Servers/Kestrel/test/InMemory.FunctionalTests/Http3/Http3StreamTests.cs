53bc77ef1a1668b4c5878fafe785986e82590f6b
James Newton-King <james@newtonking.com>
2021-01-27 19:48:03 +1300

    Connection error for invalid HTTP/3 frame on request stream

--- src/Servers/Kestrel/test/InMemory.FunctionalTests/Http3/Http3StreamTests.cs
+++ src/Servers/Kestrel/test/InMemory.FunctionalTests/Http3/Http3StreamTests.cs
@@ -1613,5 +1613,34 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
             clientTcs.SetResult(0);
             await appTcs.Task;
         }
+
+        [Theory]
+        [InlineData(nameof(Http3FrameType.MaxPushId))]
+        [InlineData(nameof(Http3FrameType.Settings))]
+        [InlineData(nameof(Http3FrameType.CancelPush))]
+        [InlineData(nameof(Http3FrameType.GoAway))]
+        public async Task UnexpectedRequestFrame(string frameType)
+        {
+            var requestStream = await InitializeConnectionAndStreamsAsync(_echoApplication);
+
+            var frame = new Http3RawFrame();
+            frame.Type = Enum.Parse<Http3FrameType>(frameType);
+            await requestStream.SendFrameAsync(frame, Memory<byte>.Empty);
+
+            await requestStream.WaitForStreamErrorAsync(Http3ErrorCode.UnexpectedFrame, expectedErrorMessage: CoreStrings.FormatHttp3ErrorUnsupportedFrameOnRequestStream(frame.FormattedType));
+        }
+
+        [Theory]
+        [InlineData(nameof(Http3FrameType.PushPromise))]
+        public async Task UnexpectedServerFrame(string frameType)
+        {
+            var requestStream = await InitializeConnectionAndStreamsAsync(_echoApplication);
+
+            var frame = new Http3RawFrame();
+            frame.Type = Enum.Parse<Http3FrameType>(frameType);
+            await requestStream.SendFrameAsync(frame, Memory<byte>.Empty);
+
+            await requestStream.WaitForStreamErrorAsync(Http3ErrorCode.UnexpectedFrame, expectedErrorMessage: CoreStrings.FormatHttp3ErrorUnsupportedFrameOnServer(frame.FormattedType));
+        }
     }
 }
