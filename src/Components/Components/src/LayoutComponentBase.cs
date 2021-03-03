d211fe1121f3742ae5dbcaf01967953b6c1a7606
Pranav K <prkrishn@hotmail.com>
2021-03-03 10:53:35 -0800

    Prevent LayoutComponentBase properties from being trimmed.
    The usage pattern for LayoutComponentBase does not really play well with static analysis and results in the
    property setter f

--- src/Components/Components/src/LayoutComponentBase.cs
+++ src/Components/Components/src/LayoutComponentBase.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
+
 namespace Microsoft.AspNetCore.Components
 {
     /// <summary>
@@ -17,5 +19,20 @@ namespace Microsoft.AspNetCore.Components
         /// </summary>
         [Parameter]
         public RenderFragment? Body { get; set; }
+
+        /// <inheritdoc />
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            // Derived instances of LayoutComponentBase do not appear in any statically analyzable
+            // calls of OpenComponent<T> where T is well-known. As a result, relying on reflection
+            // (ComponentProperties.SetProperties) will result in the setter being trimmed away since
+            // it appears unused. Statically reference it to ensure the setter is preserved.
+            if (parameters.TryGetValue(BodyPropertyName, out RenderFragment? body))
+            {
+                Body = body;
+            }
+
+            return base.SetParametersAsync(parameters);
+        }
     }
 }
